b385bd59fa63aaeac5c404d96ac873bc6938d48a
Barning Dango <85763352+Kanimiso221@users.noreply.github.com>
2024-04-02 03:06:14 +0900

    NPCを作れるように

--- toolJS.js
+++ toolJS.js
@@ -1,4 +1,4 @@
-// 
+// イベントが飛んできたときに動くもの
 document.addEventListener('DOMContentLoaded', () => {
     const dropZone = document.getElementById('dropZone');
     const resultDiv = document.getElementById('result');
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const results    = {};
 
                 paragraphs.forEach((p) => {
-                    if (p.textContent.includes('CCB') || p.textContent.includes('【正気度ロール】')) {
+                    if (p.textContent.includes('ccb') || p.textContent.includes('CCB') || p.textContent.includes('【正気度ロール】')) {
                         const characterName = p.querySelector('span:nth-child(2)').textContent.trim();
                         const resultText    = p.querySelector('span:nth-child(3)').textContent;
                         const resultParts   = resultText.split(' ＞ ');
@@ -91,10 +91,178 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// 文字列の長さを合わせる関数
+function openTab(evt, tabName) {
+    // すべてのタブコンテンツを非表示にする
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // すべてのタブリンクを非アクティブにする
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // 選択されたタブコンテンツを表示し、タブをアクティブにする
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById('npcForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // フォームの送信を阻止
+
+    const formData  = new FormData(this);
+
+    const name      = formData.get('name');
+    const hp        = formData.get('hp') || Math.floor((parseInt(formData.get('con')) + parseInt(formData.get('siz'))) / 2);
+    const mp        = formData.get('mp') || parseInt(formData.get('pow'));
+    const san       = formData.get('san') || parseInt(formData.get('pow')) * 5;
+    const str       = formData.get('str');
+    const con       = formData.get('con');
+    const pow       = formData.get('pow');
+    const dex       = formData.get('dex');
+    const app       = formData.get('app');
+    const siz       = formData.get('siz');
+    const int       = formData.get('int');
+    const edu       = formData.get('edu');
+    const addAttack = damageBonus(str, siz);
+
+    let command = '1d100<={SAN} 【正気度ロール】\n' +
+                  'CCB<={INT}*5 【アイデア】\n' +
+                  'CCB<={POW}*5 【幸運】\n' +
+                  'CCB<={EDU}*5 【知識】\n';
+
+    const skillsDiv     = document.getElementById('skills');
+    const skillElements = skillsDiv.getElementsByClassName('skillSet');
+    for (let i = 0; i < skillElements.length; i++) {
+        const skillNameElement  = skillElements[i].querySelector('.skillName');
+        const skillValueElement = skillElements[i].querySelector('.skillValue');
+        if (skillNameElement && skillValueElement && skillNameElement.value && skillValueElement.value) {
+            command += `CCB<=${skillValueElement.value} 【${skillNameElement.value}】\n`;
+        }
+    }
+
+    command += '1d3' + addAttack + ' 【ダメージ判定】\n' +
+               '1d4' + addAttack + ' 【ダメージ判定】\n' +
+               '1d6' + addAttack + ' 【ダメージ判定】\n' +
+               'CCB<={STR}5 【STR × 5】\n' +
+               'CCB<={CON}5 【CON × 5】\n' +
+               'CCB<={POW}5 【POW × 5】\n' +
+               'CCB<={DEX}5 【DEX × 5】\n' +
+               'CCB<={APP}5 【APP × 5】\n' +
+               'CCB<={SIZ}5 【SIZ × 5】\n' +
+               'CCB<={INT}5 【INT × 5】\n' +
+               'CCB<={EDU}5 【EDU × 5】\n';
+
+    // JSONデータを作成
+    const npcData = {
+        kind: "character",
+        data: {
+            name: name,
+            initiative: dex,
+            externalUrl: null,
+            iconUrl: null,
+            commands: command,
+            status: [{
+                label: "HP",
+                value: hp,
+                max: hp
+            },{
+                label: "MP",
+                value: mp,
+                max: mp
+            },{
+                label: "SAN",
+                value: san,
+                max: san
+            }],
+            params: [{
+                label: "STR",
+                value: str
+            },{
+                label: "CON",
+                value: con
+            },{
+                label: "POW",
+                value: pow
+            },{
+                label: "DEX",
+                value: dex
+            },{
+                label: "APP",
+                value: app
+            },{
+                label: "SIZ",
+                value: siz
+            },{
+                label: "INT",
+                value: int
+            },{
+                label: "EDU",
+                value: edu
+            }]
+        }
+    };
+
+    const npcDataString = JSON.stringify(npcData, null, 2);
+
+    navigator.clipboard.writeText(npcDataString).then(function() {
+
+        alert('NPCデータをクリップボードにコピーしました。');
+    }).catch(function(error) {
+
+        console.error('クリップボードにコピーできませんでした。', error);
+    });
+});
+ 
+document.getElementById('addSkill').addEventListener('click', function() {
+    const skillsDiv           = document.getElementById('skills');
+    const allSkills           = skillsDiv.querySelectorAll('.skillSet');
+    const newSkillNumber      = allSkills.length + 1;
+
+    const newSkillLabel       = document.createElement('label');
+    newSkillLabel.textContent = `技能${newSkillNumber}：`;
+    newSkillLabel.htmlFor     = `skill${newSkillNumber}`;
+
+    const newSkillInput       = document.createElement('input');
+    newSkillInput.type        = 'text';
+    newSkillInput.id          = `skill${newSkillNumber}`;
+    newSkillInput.name        = `skill${newSkillNumber}`;
+    newSkillInput.placeholder = "技能名";
+    newSkillInput.className   = "skillName";
+
+    const newSkillValue       = document.createElement('input');
+    newSkillValue.type        = 'number';
+    newSkillValue.id          = `value${newSkillNumber}`;
+    newSkillValue.name        = `value${newSkillNumber}`;
+    newSkillValue.placeholder = "技能成功値";
+    newSkillValue.className   = "skillValue";
+    newSkillValue.min         = "0";
+    newSkillValue.max         = "100";
+
+    const skillContainer = document.createElement('div');
+    skillContainer.className  = 'skillSet';
+
+    skillContainer.appendChild(newSkillLabel);
+    skillContainer.appendChild(newSkillInput);
+    skillContainer.appendChild(newSkillValue);
+
+    skillsDiv.appendChild(skillContainer);
+});
+
 function padString(str, length) {
     while (str.length < length) {
         str += ' ';
     }
     return str;
+}
+
+function damageBonus(str, siz) {
+    const add = str + siz;
+    if (2 <= add || add <= 12)       return "-1d6";
+    else if (13 <= add || add <= 16) return "-1d4";
+    else if (17 <= add || add <= 24) return "+0";
+    else if (25 <= add || add <= 32) return "+1d4";
+    else if (33 <= add || add <= 40) return "+1d6";
 }
